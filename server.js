d6d066072951b82577f1707bbc62283b1ee5d2fb
toshitsom <tsom.97@gmail.com>
2016-11-12 00:35:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,39 +30,39 @@ function createTemplate (data) {
     
     var htmlTemplate = `
     <html>
-      <head>
-          <title>
-              ${title}
-          </title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/ui/style.css" rel="stylesheet" />
-      </head> 
-      <body>
-          <div class="container">
-              <div>
-                  <a href="/">Home</a>
-              </div>
-              <hr/>
-              <h3>
-                  ${heading}
-              </h3>
-              <div>
-                  ${date.toDateString()}
-              </div>
-              <div>
-                ${content}
-              </div>
-              <hr/>
-              <h4>Comments</h4>
-              <div id="comment_form">
-              </div>
-              <div id="comments">
-                <center>Loading comments...</center>
-              </div>
-          </div>
-          <script type="text/javascript" src="/ui/article.js"></script>
-      </body>
-    </html>
+
+<head>
+    <title>Articles</title>
+    <link rel="stylesheet" type="text/css" href="blogstyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Shrikhand" rel="stylesheet">
+</head>
+
+<body>
+    <div id="header">
+        <span id="home"><a class="anchor" href="/">Home</a></span>
+        <span id="heading">Blog Dynasty</span>
+        <div id=< div id="userinfo">
+
+        </div>
+    </div>
+    <div id="list">
+    
+    </div>
+<div id="articlebody">
+${articlebody}
+<div id="comment_form">
+    
+</div>
+<div id="comments">
+    
+</div>
+</div>
+<script type="text/javascript" src="/ui/article.js"></script>
+</body>
+
+</html>
+          
+      
     `;
     return htmlTemplate;
 }
